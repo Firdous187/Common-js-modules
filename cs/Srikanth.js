f82cb3c0eb5bf1e473f75013eb395cee28d2646f
Firdous187 <Shaik.firdous0011@gmail.com>
2024-09-01 18:21:02 +0530

    Add files via upload

---
+++ cs/Srikanth.js
@@ -0,0 +1,9 @@
+let movieName ="Srikanth";
+let movieDirector ="Tushar Hiranandani";
+let moiveCast =["Rajkurmar Roa","Alaya F","fi"];
+
+let aboutMoive =()=>{
+    console.log("superhit movie");
+};
+
+module.exports= {movieDirector,movieName,moiveCast,aboutMoive};
---
+++ cs/server.js
@@ -0,0 +1,6 @@
+let movieObj=require("./Srikanth");
+
+console.log(movieObj.moiveCast);
+console.log(movieObj.movieDirector);
+console.log(movieObj.movieName);
+movieObj.aboutMoive();
